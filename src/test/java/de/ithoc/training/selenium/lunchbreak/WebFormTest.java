9c0535b787733df034eeda6b859d11b906bf91a7
BennyLutzBrights <benjamin.lutz936@gmail.com>
2023-04-20 10:53:18 +0200

    initial Setup Done

--- src/test/java/de/ithoc/training/selenium/lunchbreak/WebFormTest.java
+++ src/test/java/de/ithoc/training/selenium/lunchbreak/WebFormTest.java
@@ -1,6 +1,8 @@
 package de.ithoc.training.selenium.lunchbreak;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -11,19 +13,27 @@ import java.time.Duration;
 
 public class WebFormTest {
 
-    private WebDriver driver;
+    private WebDriver chromeDriver;
 
     @BeforeEach
     public void setUp() {
         // Create driver with the option to allow remote origins.
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
-        driver = new ChromeDriver(options);
-
-        // Navigate to this example website which is a form.
-        driver.get("https://www.selenium.dev/selenium/web/web-form.html");
+        chromeDriver = new ChromeDriver(options);
 
         // Wait a little to ensure website has been loaded.
-        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(500));
+        chromeDriver.manage().timeouts().implicitlyWait(Duration.ofMillis(500));
+    }
+
+    @AfterEach
+    public void tearDown() {
+        chromeDriver.quit();
+    }
+
+    @Test
+    public void testWebFormPage() {
+        // Navigate to this example website which is a form.
+        chromeDriver.get("https://www.selenium.dev/selenium/web/web-form.html");
     }
 }
